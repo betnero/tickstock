fcfa45cd5f7684acb5da25da9367e654d0d82012
Paweł Betnerowicz <43805630+betnero@users.noreply.github.com>
2024-07-11 18:01:19 +0200

    Create data_downloader.py
    First approach to developing a feature aimed to download data and place it in a csv file.

---
+++ data_downloader.py
@@ -0,0 +1,33 @@
+
+# Downloads price data for a stock ticker for a defined period of time.
+def tick_fixed(period, *args):
+    import pandas as pd
+    import yfinance as yf
+# Download the data for all requested stock tickers and place in a csv file.
+    for arg in args:
+        a = yf.download(arg, period=period)
+        df = pd.DataFrame(a)
+        df.to_csv(arg+"prices.csv")
+# Inform the user about the stock ticker being added to the file.
+    for arg in args:
+        print("{} added to file.".format(arg.upper()))
+
+
+# Function call example: 
+# tick_fixed('max', 'msft', 'aapl', 'goog')
+
+# Download the data for all requested stock tickers, for a defined period of time and place in a csv file.
+def tick_custom(start, end, *args):
+    import pandas as pd
+    import yfinance as yf
+
+    for arg in args:
+        a = yf.download(arg, start=start, end=end)
+        df = pd.DataFrame(a)
+        df.to_csv(arg+"prices.csv")
+
+    for arg in args:
+        print("{} added to file.".format(arg.upper()))
+
+# Function call example: 
+# EXAMPLE: tick_fixed('max', 'msft', 'aapl', 'goog')EXAMPLE: tick_custom('2020-01-01', '2020-12-31', 'msft', 'aapl', 'goog')
