1c121113ce3fc1592e21739d7ed348c8bd041a60
betnero <pawel.betnerowicz@gmail.com>
2024-07-28 09:43:35 +0200

    Script to create sqlite DB with stock data.

--- src/modules/sqlite_DB/data_to_sql.py
+++ src/modules/sqlite_DB/data_to_sql.py
@@ -1,29 +1,39 @@
+# The script downloads ALL stock data available via yfinance library, for the maximum period available and puts it in
+# a sqlite DB.
+
 import yfinance as yf
 import sqlite3
+import re
 
-# DB connection
-conn = sqlite3.connect('stock_prices.db')
-c = conn.cursor()
+with open('tickers_cleaned.txt', 'r') as f:
+    for match in re.findall(r"'(.*?)'", f.read()):
 
-# Create a table
-c.execute('''CREATE TABLE IF NOT EXISTS stock_prices
-             (date DATE, ticker TEXT, open REAL, high REAL, low REAL, close REAL)''')
+        # DB connection
+        conn = sqlite3.connect('stock_prices.db')
+        c = conn.cursor()
 
-# Download the stock prices for AAPL
-data = yf.download('AAPL', period='max')
+        # Create a table
+        c.execute('''CREATE TABLE IF NOT EXISTS stock_prices
+                     (date DATE, ticker TEXT, open REAL, high REAL, low REAL, close REAL, adj REAL, volume REAL)''')
 
-# Get the stock prices from the dataframe
-dates = [date.strftime('%Y-%m-%d') for date in data.index]
-open_prices = data['Open'].tolist()
-high_prices = data['High'].tolist()
-low_prices = data['Low'].tolist()
-close_prices = data['Close'].tolist()
+        # Download the stock prices for 'match'
+        data = yf.download(match, period='max')
 
-# Upload data into the database
-stock_prices = [(date, 'AAPL', open_price, high_price, low_price, close_price)
-                for date, open_price, high_price, low_price, close_price in
-                zip(dates, open_prices, high_prices, low_prices, close_prices)]
-c.executemany('INSERT INTO stock_prices VALUES (?,?,?,?,?,?)', stock_prices)
+        # Get the stock prices from the dataframe
+        dates = [date.strftime('%Y-%m-%d') for date in data.index]
+        open_prices = data['Open'].tolist()
+        high_prices = data['High'].tolist()
+        low_prices = data['Low'].tolist()
+        close_prices = data['Close'].tolist()
+        adj_prices = data['Adj Close'].tolist()
+        volume_prices = data['Volume'].tolist()
 
-conn.commit()
-conn.close()
+        # Upload data into the database
+        stock_prices = [(date, match, open_price, high_price, low_price, close_price, adj_price, volume_price )
+                        for date, open_price, high_price, low_price, close_price, adj_price, volume_price in
+                        zip(dates, open_prices, high_prices, low_prices, close_prices, adj_prices, volume_prices)]
+        c.executemany('INSERT INTO stock_prices VALUES (?,?,?,?,?,?,?,?)', stock_prices)
+
+        conn.commit()
+        conn.close()
+# Data categories: Date,Open,High,Low,Close,Adj Close,Volume
